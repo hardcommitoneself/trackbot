a4d4b96829c36248ce11498bdd393637c6e702e3
Jeff Hansen <jhansen@hivalley.com>
2022-05-24 01:35:53 -0600

    More styles

--- web/pages/_dev/style.vue
+++ web/pages/_dev/style.vue
@@ -23,7 +23,10 @@
         </li>
         <li>Tailwind CSS - use {primary, secondary, tertiary} for colors.</li>
         <li>
-          Icons - <a target="_blank" href="https://heroicons.com">heroicons</a>
+          Icons -
+          <a target="_blank" href="https://heroicons.com"
+            >heroicons. Use outlines. space-x-1 between icon and text.</a
+          >
         </li>
         <li>
           Paddings / Margins - x = evens, y = odds.
@@ -45,7 +48,7 @@
             <!-- dark mode -->
             <div class="font-mono text-2xl">Dark Mode Preview</div>
 
-            <div class="mt-3 font-mono">Colors</div>
+            <div class="mt-9 font-mono">Colors</div>
             <div class="mt-3 bg-primary-500 text-white px-2">
               Primary Color (buttons and links)
             </div>
@@ -56,7 +59,7 @@
               Tertiary Color (special)
             </div>
 
-            <div class="mt-4 font-mono">Headings</div>
+            <div class="mt-9 font-mono">Headings</div>
             <div class="space-y-1">
               <H1>Heading 1 (page)</H1>
               <H2>Heading 2 (section)</H2>
@@ -64,7 +67,7 @@
               <H4>Heading 4 (sub-sub-section)</H4>
             </div>
 
-            <div class="mt-4 font-mono">Text</div>
+            <div class="mt-9 font-mono">Text</div>
             <div class="space-y-1">
               <p class="font-light">Light text. This is light text.</p>
               <p>Normal text. This is just normal text.</p>
@@ -73,6 +76,31 @@
               </p>
               <p class="font-bold">Bold text is bolder</p>
               <NuxtLink class="bot-href">This is a text link</NuxtLink>
+            </div>
+
+            <div class="mt-9 font-mono">Buttons</div>
+            <div class="flex items-center space-x-4">
+              <NuxtLink class="bot-button">Button</NuxtLink>
+              <NuxtLink class="bot-alt-button">Alt Button</NuxtLink>
+              <NuxtLink class="bot-danger-button">
+                <div class="flex items-center space-x-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                    />
+                  </svg>
+                  <p>Danger</p>
+                </div>
+              </NuxtLink>
             </div>
           </div>
         </div>
