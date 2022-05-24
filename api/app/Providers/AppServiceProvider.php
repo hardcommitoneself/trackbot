4838eedc06f80ade5d408a778bb125c2b3b2cb01
Jeff Hansen <jhansen@hivalley.com>
2022-05-23 22:42:46 -0600

    Adding Meet Tests

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -2,13 +2,14 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use TiMacDonald\JsonApi\JsonApiResource;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
      * @return void
      */
     public function register()
@@ -18,11 +19,10 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
-     *
      * @return void
      */
     public function boot()
     {
-        //
+        JsonApiResource::resolveIdUsing(fn(Model $model) => $model->uuid);
     }
 }
---
+++ api/tests/Feature/Meet/GetMeetTest.php
@@ -0,0 +1,93 @@
+<?php
+
+use App\Models\Meet;
+use Symfony\Component\HttpFoundation\Response;
+
+use function Pest\Laravel\getJson;
+
+it('should return 404 if a meet is not found', function () {
+    getJson(route('meets.show', ['meet' => 'not-exists']))
+        ->assertStatus(Response::HTTP_NOT_FOUND);
+});
+
+it('show get a single meet', function () {
+    $meet = Meet::factory()->create([
+        'name' => 'Skyhawk Invite',
+    ]);
+
+    $meetResponse = getJson(route('meets.show', compact('meet')))
+        ->json('data');
+
+    expect($meetResponse)
+        ->id->toBe($meet->uuid)
+        ->attributes->name->toBe('Skyhawk Invite');
+});
+
+it('should return all meets', function () {
+    Meet::factory()->sequence(
+        ['name' => 'Skyhawk Invite'],
+        ['name' => 'Utah County Invitational']
+    )
+        ->count(2)
+        ->create();
+
+    $meetResponse = getJson(route('meets.index'))
+        ->json('data');
+
+    expect($meetResponse)->sequence(
+        fn($meet) => $meet->attributes->name->toBe('Skyhawk Invite'),
+        fn($meet) => $meet->attributes->name->toBe('Utah County Invitational'),
+    );
+});
+
+it('should sort meets based on name descending', function () {
+    Meet::factory()->sequence(
+        ['name' => 'Skyhawk Invite'],
+        ['name' => 'Utah County Invitational']
+    )
+        ->count(2)
+        ->create();
+
+    $meetResponse = getJson(route('meets.index', ['sort' => '-name']))
+        ->json('data');
+
+    expect($meetResponse)->sequence(
+        fn($meet) => $meet->attributes->name->toBe('Utah County Invitational'),
+        fn($meet) => $meet->attributes->name->toBe('Skyhawk Invite'),
+    );
+});
+
+it('should filter meets based on sport', function () {
+    Meet::factory()->sequence(
+        ['name' => 'Skyhawk Invite'],
+        ['name' => 'Utah County Invitational'],
+        ['name' => 'XC Meet', 'sport' => 'XC']
+    )
+        ->count(3)
+        ->create();
+
+    $meetResponse = getJson(route('meets.index', ['filter[sport]' => 'TRACK']))
+        ->json('data');
+
+    expect($meetResponse)->sequence(
+        fn($meet) => $meet->attributes->name->toBe('Skyhawk Invite'),
+        fn($meet) => $meet->attributes->name->toBe('Utah County Invitational'),
+    );
+});
+
+it('should filter meets based on name', function () {
+    Meet::factory()->sequence(
+        ['name' => 'Skyhawk Invite'],
+        ['name' => 'Utah County Invitational'],
+        ['name' => 'XC Meet', 'sport' => 'XC']
+    )
+        ->count(3)
+        ->create();
+
+    $meetResponse = getJson(route('meets.index', ['filter[name]' => 'XC']))
+        ->json('data');
+
+    expect($meetResponse)->sequence(
+        fn($meet) => $meet->attributes->name->toBe('XC Meet'),
+    );
+});
