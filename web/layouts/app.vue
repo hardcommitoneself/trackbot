25e200918004f4f54fc8d725aa5468fe4b563a64
Theodore <theodoreyaosin@gmail.com>
2022-05-25 07:55:35 -0700

    Setup app layout

---
+++ web/layouts/app.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <LayoutTheNavigation />
+        <slot />
+    </div>
+</template>
--- web/pages/home/index.vue
+++ web/pages/home/index.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        <LayoutTheNavigation />
-    </div>
+    <NuxtLayout name="app">
+        
+    </NuxtLayout>
 </template>
+<script setup>
+import {defineComponent} from "vue"
+
+definePageMeta({
+    layout: false
+})
+</script>
