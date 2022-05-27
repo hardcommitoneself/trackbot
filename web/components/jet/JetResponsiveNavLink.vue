8639a98ae97777939eed29b3dd4bc8c75b961dc2
Theodore <theodoreyaosin@gmail.com>
2022-05-27 05:21:43 -0700

    Add cursor pointer in responsive nav link

--- web/components/jet/JetResponsiveNavLink.vue
+++ web/components/jet/JetResponsiveNavLink.vue
@@ -16,8 +16,8 @@ export default defineComponent({
     computed: {
         classes() {
             return (this.active ?? false)
-                ? 'block py-2 text-base px-4 text-white font-semibold bg-tertiary-500 transition'
-                : 'block py-2 text-base px-4 text-gray-600 dark:text-gray-300 hover:text-gray-800 hover:bg-primary-500 dark:hover:text-gray-800 dark:hover:bg-primary-500 dark:bg-gray-800 hover:border-gray-300 transition'
+                ? 'cursor-pointer block py-2 text-base px-4 text-white font-semibold bg-tertiary-500 transition'
+                : 'cursor-pointer block py-2 text-base px-4 text-gray-600 dark:text-gray-300 hover:text-gray-800 hover:bg-primary-500 dark:hover:text-gray-800 dark:hover:bg-primary-500 dark:bg-gray-800 hover:border-gray-300 transition'
         }
     }
 })
