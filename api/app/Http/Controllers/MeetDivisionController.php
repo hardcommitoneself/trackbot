1ec3e622dfc1018a1434295f803d1673c5c434c5
Jeff Hansen <jhansen@hivalley.com>
2022-05-29 08:51:49 -0600

    Add MeetDivision

---
+++ api/app/Http/Controllers/MeetDivisionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMeetDivisionRequest;
+use App\Http\Requests\UpdateMeetDivisionRequest;
+use App\Models\MeetDivision;
+
+class MeetDivisionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreMeetDivisionRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMeetDivisionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MeetDivision  $meetDivision
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MeetDivision $meetDivision)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MeetDivision  $meetDivision
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MeetDivision $meetDivision)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateMeetDivisionRequest  $request
+     * @param  \App\Models\MeetDivision  $meetDivision
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateMeetDivisionRequest $request, MeetDivision $meetDivision)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MeetDivision  $meetDivision
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MeetDivision $meetDivision)
+    {
+        //
+    }
+}
