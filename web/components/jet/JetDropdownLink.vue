fc532ca26a1c351670269062c3447df521d72717
Jeff Hansen <jhansen@hivalley.com>
2022-05-30 22:48:43 -0600

    Replacing indigo with tertiary

--- web/components/jet/JetDropdownLink.vue
+++ web/components/jet/JetDropdownLink.vue
@@ -1,22 +1,24 @@
 <template>
-    <a :class="[classes, 'block px-4 py-2 mb-0.5 text-sm leading-5 transition']">
-        <slot />
-    </a>
+  <a :class="[classes, 'block px-4 py-2 mb-0.5 text-sm leading-5 transition']">
+    <slot />
+  </a>
 </template>
 <script>
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    props: {
-        active: {
-            type: Boolean,
-            defalut: false
-        }
+  props: {
+    active: {
+      type: Boolean,
+      defalut: false,
     },
-    computed: {
-        classes() {
-            return this.active ? 'font-semibold bg-indigo-500 text-white hover:bg-indigo-500 hover:bg-indigo-500' : 'text-gray-700 dark:text-gray-400 hover:bg-primary-500 hover:text-white hover:dark:text-white';
-        }
-    }
-})
+  },
+  computed: {
+    classes() {
+      return this.active
+        ? "font-semibold bg-tertiary-500 text-white hover:bg-tertiary-500 hover:bg-tertiary-500"
+        : "text-gray-700 dark:text-gray-400 hover:bg-primary-500 hover:text-white hover:dark:text-white";
+    },
+  },
+});
 </script>
