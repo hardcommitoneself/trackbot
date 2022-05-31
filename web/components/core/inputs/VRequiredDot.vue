6eb98562e302e0aec45e114a35927fe400469249
Theodore <theodoreyaosin@gmail.com>
2022-05-31 13:41:39 -0700

    Add some components

---
+++ web/components/core/inputs/VRequiredDot.vue
@@ -0,0 +1,7 @@
+<template>
+    <span >
+        <svg class="mr-2 ml-1 h-2 w-2 text-red-500" fill="currentColor" viewBox="0 0 8 8" >
+            <circle cx="4" cy="4" r="3" />
+        </svg >
+    </span >
+</template>
---
+++ web/components/jet/JetCheckbox.vue
@@ -0,0 +1,3 @@
+<template>
+    <input type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+</template>
