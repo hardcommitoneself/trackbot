ae549a0ed5f96141d8aa28a06a8294ef77d788f8
Jeff Hansen <jhansen@hivalley.com>
2022-05-30 22:58:47 -0600

    add divide on meet list

--- web/components/core/lists/VListStackedItemBase.vue
+++ web/components/core/lists/VListStackedItemBase.vue
@@ -1,21 +1,21 @@
 <template>
-    <li >
-        <a :href="href" class="block bot-row-hover" >
-            <div class="px-4 py-3 sm:px-6" >
-                <slot />
-            </div >
-        </a >
-    </li >
+  <li>
+    <a :href="href" class="block bot-row-hover">
+      <div class="px-4 py-3 sm:px-6">
+        <slot />
+      </div>
+    </a>
+  </li>
 </template>
 <script>
-import { defineComponent } from "vue"
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    props: {
-        href: {
-            type: String,
-            default: ""
-        }
-    }
-})
+  props: {
+    href: {
+      type: String,
+      default: "",
+    },
+  },
+});
 </script>
