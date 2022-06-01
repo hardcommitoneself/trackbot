8cc29bf5cbbefae3312de7aa1236bd6c52098a5c
Jeff Hansen <jhansen@hivalley.com>
2022-05-31 20:29:04 -0600

    fetching organization and meets - ** create .env **
    .env should contain:
    API_BASE_URL: http://localhost:8000/api/v1

--- web/components/core/header/H2.vue
+++ web/components/core/header/H2.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2 class="text-3xl font-semibold text-secondary-500">
+  <h2 class="text-2xl font-semibold text-secondary-500">
     <slot />
   </h2>
 </template>
---
+++ web/pages/meets/[meet].vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout name="app">
+    <MainMeetShow />
+  </NuxtLayout>
+</template>
+<script setup>
+import { defineComponent } from "vue";
+
+definePageMeta({
+  layout: false,
+});
+</script>
