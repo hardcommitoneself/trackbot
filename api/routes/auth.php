315ae275c654b3a38a54dc33fc96a63bb4625bb4
Theodore <theodoreyaosin@gmail.com>
2022-06-01 23:23:25 -0700

    Add api to get current user

--- api/routes/auth.php
+++ api/routes/auth.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
@@ -35,3 +36,7 @@ Route::post('/email/verification-notification', [EmailVerificationNotificationCo
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->middleware('auth')
                 ->name('logout');
+
+Route::get('/user', function() { return auth()->user(); })
+                ->middleware('auth')
+                ->name('current.auth');
